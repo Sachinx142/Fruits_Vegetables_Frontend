5174483ca46c0aa8df9af761e0cb9f615ab5acb9
Sachinx142 <mehrasachinr30@gmail.com>
2025-10-20 16:11:39 +0530

    third commit

--- src/auth.js
+++ src/auth.js
@@ -1,7 +1,7 @@
 // auth.ts
 import NextAuth from "next-auth";
 import CredentialsProvider from "next-auth/providers/credentials";
-
+ 
 export const authOptions = {
   providers: [
     CredentialsProvider({

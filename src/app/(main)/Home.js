1c4972454562bc2958ec5f20c764456a23a51028
Sachinx142 <mehrasachinr30@gmail.com>
2025-10-22 15:21:09 +0530

    22-10-2025

--- src/app/(main)/Home.js
+++ src/app/(main)/Home.js
@@ -14,8 +14,6 @@ import "swiper/css";
 import "swiper/css/navigation";
 import "swiper/css/pagination";
 
-
-
 export default function Home() {
   const router = useRouter();
   const session = useSession();
